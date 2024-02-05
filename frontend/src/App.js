9b657fabb562a75bb146c3e0890b5638f836b860
Kshitiz-Lall <kshitiz.lall@genzeon.com>
2024-02-05 21:03:16 +0530

    installed dependencies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 export default function App() {
   return (
-    <h1 className="text-2xl font-bold underline">
+    <h1 className="text-2xl font-bold underline ">
       Hello world!
     </h1>
   )
