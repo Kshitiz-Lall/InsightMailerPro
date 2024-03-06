c2064bef4ffcefc20f93eb0589e624f6a28a6aad
Kshitiz-Lall <kshitiz.lall@genzeon.com>
2024-03-06 11:45:27 +0530

    Removed FN

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,25 @@
-export default function App() {
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
   return (
-    <h1 className="text-2xl font-bold underline ">
-      Hello world!
-    </h1>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
+
+export default App;
